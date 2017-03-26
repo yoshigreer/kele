f452f04af1ab443067655c4773546880c2518e62
Trenton Greer <yoshigreer@gmail.com>
2017-03-26 11:19:31 -0600

    can retrieve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,4 +14,9 @@ class Kele
     response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
 end
